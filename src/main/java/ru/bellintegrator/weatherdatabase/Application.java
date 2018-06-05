ed9c741036c15afae069b6c331d70d627d428988
Goriachev Maksim <MGoriachev@bell-main.bellintegrator.ru>
2018-06-05 11:34:29 +0300

    Изменил файлы конфигурации

--- src/main/java/ru/bellintegrator/weatherdatabase/Application.java
+++ src/main/java/ru/bellintegrator/weatherdatabase/Application.java
@@ -1,40 +1,28 @@
 package ru.bellintegrator.weatherdatabase;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.core.task.TaskExecutor;
-import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
-import org.springframework.jms.support.converter.MessageConverter;
-import org.springframework.jms.support.converter.MessageType;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
-
-import javax.jms.ConnectionFactory;
-import javax.jms.JMSException;
 import java.util.Locale;
 
 @SpringBootApplication
-//@EnableJms
-public class Application {
-    //public final static String WEATHER_MESSAGE_QUEUE = "weather-message-queue";
-
-    //public final static String WEATHER_MESSAGE_QUEUE = "ExpiryQueue";
-
-    public static void main(String[] args) {
-//        SpringApplication app = new SpringApplication(Application.class);
-//        app.run(args);
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+@EnableTransactionManagement
+public class Application  extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
     @Bean
     public TaskExecutor controllerPool() {
@@ -50,26 +38,4 @@ public class Application {
         sessionLocaleResolver.setDefaultLocale(Locale.US);
         return sessionLocaleResolver;
     }
-
-
-
-
-//    // Бины для jms
-//    @Bean
-//    public JmsListenerContainerFactory<?> jmsFactory(ConnectionFactory connectionFactory,
-//                                                     DefaultJmsListenerContainerFactoryConfigurer configurer) {
-//        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-//
-//        configurer.configure(factory, connectionFactory);
-//        return factory;
-//    }
-//
-//    // Сериализация контента сообщений в json
-//    @Bean
-//    public MessageConverter jacksonJmsMessageConverter() {
-//        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-//        converter.setTargetType(MessageType.TEXT);
-//        converter.setTypeIdPropertyName("_type");
-//        return converter;
-//    }
 }
--- src/main/java/ru/bellintegrator/weatherdatabase/ReceiverConfig.java
+++ src/main/java/ru/bellintegrator/weatherdatabase/ReceiverConfig.java
@@ -1,46 +1,93 @@
 package ru.bellintegrator.weatherdatabase;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.icatch.jta.UserTransactionManager;
+import com.atomikos.jdbc.AtomikosDataSourceBean;
+import com.atomikos.jms.AtomikosConnectionFactoryBean;
+import org.apache.activemq.ActiveMQXAConnectionFactory;
+import org.postgresql.xa.PGXADataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.annotation.JmsListenerConfigurer;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
 import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerEndpointRegistrar;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
 import org.springframework.jms.support.converter.MessageType;
-import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
-
+import org.springframework.transaction.jta.JtaTransactionManager;
 import javax.jms.ConnectionFactory;
-
+import javax.sql.DataSource;
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
 
 @Configuration
 @EnableJms
 public class ReceiverConfig {
-//    @Value("${activemq.broker-url}")
-//    private String brokerUrl;
-//    @Value("${activemq.user}")
-//    private String userName;
-//    @Value("${activemq.password}")
-//    private String password;
 
-    /*
-     * Initial ConnectionFactory
-     */
-//    @Bean
-//    public ConnectionFactory connectionFactory(){
-//        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory();
-//        //connectionFactory.setBrokerURL(brokerUrl);
-//        //connectionFactory.setUserName(userName);
-//        //connectionFactory.setPassword(password);
-//        return connectionFactory;
-//    }
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+    @Value("${spring.activemq.broker-url}")
+    private String brokerUrl;
 
-    @Bean // Serialize message content to json using TextMessage
+
+    @Bean
+    public UserTransaction userTransaction() throws Throwable {
+        UserTransactionImp userTransactionImp = new UserTransactionImp();
+        userTransactionImp.setTransactionTimeout(1000);
+        return userTransactionImp;
+    }
+
+    @Bean(initMethod = "init", destroyMethod = "close")
+    public TransactionManager transactionManager() throws Throwable {
+        UserTransactionManager userTransactionManager = new UserTransactionManager();
+        userTransactionManager.setForceShutdown(false);
+
+        return userTransactionManager;
+    }
+
+    @Bean
+    public JtaTransactionManager jtaTransactionManager() throws Throwable {
+        JtaTransactionManager transactionManager = new JtaTransactionManager();
+        transactionManager.setTransactionManager(transactionManager());
+        transactionManager.setUserTransaction(userTransaction());
+        return transactionManager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        PGXADataSource pgXaDataSource = new PGXADataSource();
+        pgXaDataSource.setUrl(datasourceUrl);
+        pgXaDataSource.setUser(username);
+        pgXaDataSource.setPassword(password);
+
+        AtomikosDataSourceBean xaDataSource = new AtomikosDataSourceBean();
+        xaDataSource.setXaDataSource(pgXaDataSource);
+        xaDataSource.setUniqueResourceName("PostgresXADS");
+        xaDataSource.setXaDataSourceClassName("org.postgresql.xa.PGXADataSource");
+
+        return xaDataSource;
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        ActiveMQXAConnectionFactory activeMQXAConnectionFactory = new ActiveMQXAConnectionFactory();
+        activeMQXAConnectionFactory.setBrokerURL(brokerUrl);
+
+        AtomikosConnectionFactoryBean atomikosConnectionFactoryBean = new AtomikosConnectionFactoryBean();
+        atomikosConnectionFactoryBean.setUniqueResourceName("ActiveMQXADS");
+        atomikosConnectionFactoryBean.setLocalTransactionMode(false);
+        atomikosConnectionFactoryBean.setXaConnectionFactory(activeMQXAConnectionFactory);
+        return atomikosConnectionFactoryBean;
+    }
+
+
+    @Bean // Сериализация сообщений в json
     public MessageConverter jacksonJmsMessageConverter() {
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setTargetType(MessageType.TEXT);
@@ -48,45 +95,19 @@ public class ReceiverConfig {
         return converter;
     }
 
-
-    /*
-     * Used for Receiving Message
-     */
-    @Bean
+    @Bean // для принятия сообщений
     public JmsListenerContainerFactory<?> jmsFactory(ConnectionFactory connectionFactory,
-                                                     DefaultJmsListenerContainerFactoryConfigurer configurer) {
+                                                     DefaultJmsListenerContainerFactoryConfigurer configurer) throws Throwable {
         DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+
+        factory.setConcurrency("1-1");
+        factory.setSessionTransacted(true);
+        factory.setPubSubDomain(true);
+        factory.setTransactionManager(jtaTransactionManager());
+
         factory.setMessageConverter(jacksonJmsMessageConverter());
         configurer.configure(factory, connectionFactory);
+
         return factory;
     }
-
-
-
-
-
 }
-//@Configuration
-//@EnableJms
-//public class ReceiverConfig {
-//
-//    @Value("${activemq.broker-url}")
-//    private String brokerUrl;
-//
-//    @Bean
-//    public ActiveMQConnectionFactory activeMQConnectionFactory() {
-//        ActiveMQConnectionFactory activeMQConnectionFactory = new ActiveMQConnectionFactory();
-//        activeMQConnectionFactory.setBrokerURL(brokerUrl);
-//
-//        return activeMQConnectionFactory;
-//    }
-//
-//    @Bean
-//    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
-//        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
-//        factory.setConnectionFactory(activeMQConnectionFactory());
-//        factory.setConcurrency("3-10");
-//
-//        return factory;
-//    }
-//}
